cf77a33a579be32c230bd615221d847d81a2e64e
Oana Berentan <oana.berentan@yahoo.com>
2019-04-18 19:22:05 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,12 @@
 def turn(board, index)
 
-    def display_board
-
-    end
+  def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
 
     def valid_move?(board, index)
     def position_taken?(array, ind)
